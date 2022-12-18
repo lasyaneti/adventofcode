d1b63b3e805950fc6bfbf4124fbfac32ecb2bb3e
Lasya <neti.lasya@gmail.com>
2022-12-18 13:30:27 -0800

    day8

---
+++ 2022/day8/day8-1.py
@@ -0,0 +1,60 @@
+def treesVisible():
+    # all trees in border are visible
+    # find trees visible in interior from ANY direction? 
+    with open("day8.dat", "r") as f:
+        lines = f.readlines()
+        visibility = []
+        for line in lines:
+            line = line.rstrip()
+            tmp = []
+            for x in line:
+                tmp.append(False)
+            visibility.append(tmp)
+        
+        # set border visibility
+        for i in range(len(lines)):
+            lines[i] = lines[i].rstrip()
+            for j in range(len(lines[i])):
+                if i == 0 or (i == len(lines) - 1) or j == 0 or (j == len(lines[i]) - 1):
+                    visibility[i][j] = True
+
+        # check interior visibility  
+        for i in range(1, len(lines) - 1):
+            lines[i] = lines[i].rstrip()
+            for j in range(1, len(lines[i]) - 1):
+
+                tempj = j + 1
+                keeplooking = True
+                while tempj < len(lines[i]) and keeplooking:
+                    if lines[i][tempj] >= lines[i][j]:
+                        keeplooking = False
+                    tempj += 1
+                if keeplooking: visibility[i][j] = True
+
+                tempj = j - 1
+                keeplooking = True
+                while tempj >= 0 and keeplooking:
+                    if lines[i][tempj] >= lines[i][j]:
+                        keeplooking = False
+                    tempj -= 1
+                if keeplooking: visibility[i][j] = True
+
+                tempi = i - 1
+                keeplooking = True
+                while tempi >= 0 and keeplooking:
+                    if lines[tempi][j] >= lines[i][j]:
+                        keeplooking = False
+                    tempi -= 1
+                if keeplooking: visibility[i][j] = True
+
+                tempi = i + 1
+                keeplooking = True
+                while tempi < len(lines) and keeplooking:
+                    if lines[tempi][j] >= lines[i][j]:
+                        keeplooking = False
+                    tempi += 1
+                if keeplooking: visibility[i][j] = True
+
+        return sum(item.count(True) for item in visibility)
+
+print(treesVisible())
---
+++ 2022/day8/day8-2.py
@@ -0,0 +1,61 @@
+def highestScenicScore():
+    # all trees in border are visible
+    # find trees visible in interior from ANY direction? 
+    with open("day8.dat", "r") as f:
+        lines = f.readlines()
+        maxScore = 0
+
+        for i in range(len(lines)):
+            lines[i] = lines[i].rstrip()
+            for j in range(len(lines[i])):
+                
+                rightScore = 0
+                tempj = j + 1
+                keeplooking = True
+                while tempj < len(lines[i]) and keeplooking:
+                    if lines[i][tempj] < lines[i][j]:
+                        rightScore += 1
+                    else: 
+                        rightScore += 1
+                        keeplooking = False
+                    tempj += 1
+
+                leftScore = 0
+                tempj = j - 1
+                keeplooking = True
+                while tempj >= 0 and keeplooking:
+                    if lines[i][tempj] < lines[i][j]:
+                        leftScore += 1
+                    else: 
+                        leftScore += 1
+                        keeplooking = False
+                    tempj -= 1
+
+                topScore = 0
+                tempi = i - 1
+                keeplooking = True
+                while tempi >= 0 and keeplooking:
+                    if lines[tempi][j] < lines[i][j]:
+                        topScore += 1
+                    else: 
+                        topScore += 1
+                        keeplooking = False
+                    tempi -= 1
+
+                downScore = 0
+                tempi = i + 1
+                keeplooking = True
+                while tempi < len(lines) and keeplooking:
+                    if lines[tempi][j] < lines[i][j]:
+                        downScore += 1
+                    else: 
+                        downScore += 1
+                        keeplooking = False
+                    tempi += 1
+            
+                if topScore * downScore * leftScore * rightScore > maxScore:
+                    maxScore =  topScore * downScore * leftScore * rightScore
+
+        return maxScore
+
+print(highestScenicScore())
